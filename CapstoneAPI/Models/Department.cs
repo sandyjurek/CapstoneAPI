821975b1f5a697ee6e200abfe40552494c5f3927
Sandy Jurek <sandyjurek@quickenloans.com>
2020-11-15 13:33:49 -0500

    Add project files.

---
+++ CapstoneAPI/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CapstoneAPI.Models
+{
+    public partial class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool? Hire { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ CapstoneAPI/Models/ProductOverviewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneAPI.Models
+{
+    public class ProductOverviewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
